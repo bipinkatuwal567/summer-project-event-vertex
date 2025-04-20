58205dbf69c848dc0bea03858a031281a3736688
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2025-04-20 13:56:47 +0545

    fix: booking model schema

--- server/model/BookingModel.js
+++ server/model/BookingModel.js
@@ -1,38 +1,43 @@
 import mongoose from "mongoose";
 
-const bookingSchema = mongoose.Schema({
-    userId: {
-        type : mongoose.Schema.Types.ObjectId, ref: "User", required: true, 
-    }, 
-    eventId: {
-        type: mongoose.Schema.Types.ObjectId, ref: "Event", 
-        required: true, 
-    },
-    ticketType: {
-        type: String, 
-        required: true,
-    }, 
-    quantity: {
-        type: Number, 
-        required: true, 
-        min: 1, 
-    }, 
-    totalPrice: {
-        type: Number, 
-        required: true, 
-    }, 
-    paymentStaute: {
-        type: String, 
-        enum: [
-            "Pending", 
-            "Paid", 
-        ], 
-        default: "Pending", 
-    }
+const bookingSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  eventId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Event",
+    required: true,
+  },
+  ticketType: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  ticketPrice: {
+    type: Number,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  paymentStatus: {
+    type: String,
+    enum: ["Pending", "Paid"],
+    default: "Pending",
+  },
 }, {
-    Timestamp: true, 
-})
+  timestamps: true,
+});
+
+bookingSchema.index({ userId: 1, eventId: 1 });
 
 const Booking = mongoose.model("Booking", bookingSchema);
-
 export default Booking;
